9779b7e6aa344f884c49c24186b49902b2c3585d
Ethan Lewis <ethanlewis787@gmail.com>
2022-06-14 21:31:04 -0700

    feat: basic layout of lib
    Signed-off-by: Ethan Lewis <ethanlewis787@gmail.com>

---
+++ src/handler.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const Request = @import("http").Request;
+const Response = @import("http").Response;
+const Method = @import("http").Method;
+
+var hanlderFn = defaulthandler;
+
+pub const HandlerFn = fn handle(Request, Response) anyerror!void;
+
+pub fn Handler(comptime handler: anytype) !void {
+    if (@TypeOf(handler) == HandlerFn) {
+        hanlderFn = handler;
+    } else {
+        hanlderFn = defaulthandler;
+    }
+}
+
+pub fn Execute() !void {
+    var request = try Request.builder(std.testing.allocator)
+        .method(Method.Get)
+        .uri("https://ziglang.org/")
+        .body("");
+    defer request.deinit();
+
+    var response = Response{body: _body,_version,_status,_headers};
+
+    try hanlderFn(request,response);
+}
+
+fn defaulthandler(_: Request, _: Response) !void {
+    std.log.info("All your codebase are belong to us. boom", .{});
+}
---
+++ src/main.zig
@@ -0,0 +1,11 @@
+
+pub const Handle = @import("handler.zig");
+pub const Request = @import("http").Request;
+pub const Response = @import("http").Response;
+
+//const std = @import("std");
+//const c = @cImport({
+//    @cInclude("stdlib.h");
+//    @cInclude("spin-http.h");
+// });
+
