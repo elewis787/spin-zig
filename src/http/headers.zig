23709158629f77cea90ac7e22cd0ae05a1066bc0
Ethan Lewis <ethanlewis787@gmail.com>
2022-06-23 22:59:55 -0700

    feat: additional fn impls for header

--- src/http/headers.zig
+++ src/http/headers.zig
@@ -9,12 +9,10 @@ pub const Headers = struct {
     const HeaderMap = StringHashMap(ArrayList([]const u8));
     header_map: HeaderMap,
     allocator: Allocator,
-    found: bool,
 
     pub fn init(allocator: Allocator) Headers {
         return Headers{
             .allocator = allocator,
-            .found = false,
             .header_map = HeaderMap.init(allocator),
         };
     }
@@ -36,32 +34,69 @@ pub const Headers = struct {
             v.value_ptr.* = list;
         } else {
             try v.value_ptr.*.append(value);
-            self.found = true;
         }
     }
 
-    pub fn get(self: Headers, key: []const u8) [][]const u8 {
+    pub fn set(self: *Headers, key: []const u8, value: []const u8) !void{
+        var v = try self.header_map.getOrPut(key);
+        //new key
+        if (!v.found_existing) {
+            var list = ArrayList([]const u8).init(self.allocator);
+            try list.append(value);
+            v.value_ptr.* = list;
+        } else {
+            try v.value_ptr.*.dinit();
+            var list = ArrayList([]const u8).init(self.allocator);
+            try list.append(value);
+            v.value_ptr.* = list;
+        }
+    }
+
+    pub fn get(self: Headers, key: []const u8) ?[]const u8 {
+        // return first value found at key
+        var list = self.header_map.get(key).?;
+        if (list.items.len > 0) {
+            return list.items[0];
+        }
+        return null;
+    }
+
+    pub fn values(self: Headers, key: []const u8) [][]const u8 {
         var list = self.header_map.get(key).?;
         return list.items;
     }
 
-    //   pub fn values(self: Headers) [][]const u8 {}
+    pub fn del(self: *Headers, key: []const u8) bool{
+        return self.header_map.remove(key);
+    }
+
 };
 
-test "basic headers operations" {
+test "multiple headers" {
     var headers = Headers.init(test_allocator);
     defer headers.deinit();
 
-    try headers.add("Accept", "Application/json");
+    try headers.add("Accept", "Application/json,Application/xml");
     try headers.add("Accept", "Application/xml");
     try std.testing.expect(headers.header_map.count() == 1);
-    try std.testing.expect(headers.found == true);
     try std.testing.expect(headers.header_map.contains("Accept"));
-  
-    var values = headers.get("Accept");
+
+    var values = headers.values("Accept");
     try std.testing.expect(values.len == 2);
-    var arStr: [2][]const u8 = [_][]const u8{ "Application/json", "Application/xml" };
+    var arStr: [2][]const u8 = [_][]const u8{ "Application/json,Application/xml", "Application/xml" };
     var test_slice = arStr[0..];
     try std.testing.expect(std.mem.eql(u8, values[0], test_slice[0]));
     try std.testing.expect(std.mem.eql(u8, values[1], test_slice[1]));
+}
+
+test "get with multi header" {
+    var headers = Headers.init(test_allocator);
+    defer headers.deinit();
+
+    try headers.add("Accept", "Application/json,Application/xml");
+    try headers.add("Accept", "Application/xml");
+    try std.testing.expect(headers.header_map.count() == 1);
+    try std.testing.expect(headers.header_map.contains("Accept"));
+
+    try std.testing.expect(std.mem.eql(u8,headers.get("Accept").?,"Application/json,Application/xml"));
 }
