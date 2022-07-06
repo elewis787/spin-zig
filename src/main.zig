c03339a92b8c1814f6848d62a893f5a03c855e0d
Ethan Lewis <ethanlewis787@gmail.com>
2022-07-06 15:47:30 -0700

    feat: basic handler flow

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,29 @@
 const int = @import("internals.zig");
 const spin = @import("spin-http.zig");
+const http = @import("http");
+const Request = http.Request;
+const Reponse = http.Response;
+const std = @import("std");
+
+
+pub const HandlerFn = fn handle([]const u8) void;
+
+pub var handler = defaultHandler;
+
+pub fn defaultHandler(v: []const u8) void {
+    _ = v;
+    std.debug.print("http handler undefined\n", .{});
+}
+
+pub fn handle(h: HandlerFn) void {
+    handler = h;
+    h("yolo");
+}
+
+fn testFn(v: []const u8) void {
+    std.debug.print("Hello, {s}!\n", .{v});
+}
 
 pub fn main() void {
-    int.spin_http_handle_http_request();
+    handle(testFn);
 }
