7fcc3ff698faaf5f0c3c58f954d99a8d8713a9b1
Ethan Lewis <ethanlewis787@gmail.com>
2022-07-17 22:34:14 -0700

    feat: read body from mem and convert from c array of u8s to zig slice

--- src/spin.zig
+++ src/spin.zig
@@ -23,7 +23,7 @@ const methods = [_][]const u8{
 
 export fn spin_http_handle_http_request(req: *c.spin_http_request_t, res: *c.spin_http_response_t) void {
     defer c.spin_http_request_free(req);
-    //const body = std.mem.asBytes(req.body.val.ptr);
+    const body = std.mem.span(req.body.val.ptr);
     const m = Method.from_bytes(methods[req.method]);
     if (@TypeOf(m) != Method) {
         return;
@@ -31,7 +31,7 @@ export fn spin_http_handle_http_request(req: *c.spin_http_request_t, res: *c.spi
 
     var request = try Request.builder(std.testing.allocator)
         .method(Method.Get)
-        .body("body");
+        .body(body);
 
     var response = try Response.builder(std.testing.allocator)
         .version(.Http11)
