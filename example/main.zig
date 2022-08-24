daf807ee5dfd3f049968f97845744b603ae51cf1
Ethan Lewis <ethanlewis787@gmail.com>
2022-08-23 22:41:07 -0700

    feat: additional debug statements, narrowed down mem out of bound to header allocation

--- example/main.zig
+++ example/main.zig
@@ -2,14 +2,18 @@ const std = @import("std");
 const spin = @import("spin");
 
 fn hello(req: *spin.Request, rw: *spin.ResponseWriter) void {
+    std.debug.print("in handler fun\n", .{});
+
     _ = req;
     rw.status(std.http.Status.ok);
     const n = rw.write("{\"foo\":\"bar\", \"baz\":\"boo\"}");
-    if (@TypeOf(n) == spin.WriteError ) {
+    if (@TypeOf(n) == spin.WriteError) {
         rw.status(std.http.Status.non_authoritative_info);
     }
 }
 
 pub fn main() void {
+    std.debug.print("in with main\n", .{});
     spin.Http.handle(hello);
+    std.debug.print("done with main\n", .{});
 }
